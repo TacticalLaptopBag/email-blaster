017cf6c9a8c0c885b543eb715adb4c85167e0e88
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2025-01-14 11:59:03 -0500

    Persistence is now mapped to guild ID

--- src/main/kotlin/MessageReceiveListener.kt
+++ src/main/kotlin/MessageReceiveListener.kt
@@ -3,56 +3,79 @@ package com.github.tacticallaptopbag.email_blaster
 import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import java.util.*
+import org.slf4j.LoggerFactory
+import java.util.Properties
 import kotlin.io.path.*
 
-const val CHANNEL_PROP_NAME = "mail-blaster.discord.announcement-channel-id"
-
 class MessageReceiveListener(private val _mailingList: MailingList) : ListenerAdapter() {
-    private val _channelPath = Dirs.dataPath.resolve("persistence.properties")
+    private val _channelPath = Dirs.dataPath.resolve("channels.properties")
     private val _channelProps = Properties()
 
-    var announcementChannelId: String? = null
-        set(value) {
-            field = value
-            _channelProps.setProperty(CHANNEL_PROP_NAME, value)
-            if(_channelPath.notExists()) {
-                _channelPath.createParentDirectories()
-                _channelPath.createFile()
-            }
-            _channelPath.outputStream().use { stream ->
-                _channelProps.store(stream, null)
-            }
-        }
+    companion object {
+        private val _logger = LoggerFactory.getLogger(MessageReceiveListener::class.java)
+    }
 
     init {
         if(_channelPath.isRegularFile()) {
             _channelPath.toFile().inputStream().use { stream ->
                 _channelProps.load(stream)
-                announcementChannelId = _channelProps.getProperty(CHANNEL_PROP_NAME)
+                _logger.debug("Loaded announcement channel IDs from file")
             }
         }
     }
 
+    private fun getPropertyName(guildId: String): String {
+        return "mail-blaster.discord.channel.$guildId"
+    }
+
+    private fun saveChannelIds() {
+        _channelPath.outputStream().use { stream ->
+            _channelProps.store(stream, null)
+            _logger.debug("Saved channel IDs")
+        }
+    }
+
+    fun setChannelId(guildId: String, channelId: String) {
+        _logger.debug("Setting channel for $guildId to $channelId")
+        _channelProps.setProperty(getPropertyName(guildId), channelId)
+        saveChannelIds()
+    }
+
+    fun deleteChannelId(guildId: String): Boolean {
+        _logger.debug("Deleting channel for $guildId")
+        val success = _channelProps.remove(guildId) != null
+        saveChannelIds()
+        return success
+    }
+
     override fun onMessageReceived(event: MessageReceivedEvent) {
-        if(announcementChannelId.isNullOrBlank()) return
-        if(event.channel.id != announcementChannelId) return
+        val channelId = _channelProps.getProperty(getPropertyName(event.guild.id))
+        _logger.debug("Message received in ${event.channel.id}, announcement channel is $channelId")
+        if(channelId.isNullOrBlank()) return
+        if(event.channel.id != channelId) return
+
+        _logger.info("Preparing email...")
 
         val rawContent = event.message.contentRaw
         val messageBody = event.message.contentDisplay
 
         val subject = if(rawContent.startsWith("#")) {
-            MailBlasterProperties.emailSubjectPrefix + rawContent.lines().first()
+            val subjectLine = rawContent.lines().first().replace("#", "").trim()
+            MailBlasterProperties.emailSubjectPrefix + subjectLine
         } else {
             MailBlasterProperties.emailSubjectPrefix + MailBlasterProperties.emailDefaultSubject
         }
 
+        _logger.debug("Adding emails to email session...")
         val session = EmailSession(subject, messageBody)
         for(email in _mailingList.loadAllEmails(event.guild.id)) {
             session.addTo(email)
         }
-        session.send()
+        _logger.debug("Emails added to session")
+        session.sendHTML()
 
-        event.message.addReaction(Emoji.fromFormatted("�")).queue()
+        event.message.addReaction(Emoji.fromUnicode("U+1F4E7")).queue()
+
+        _logger.info("Email sent")
     }
 }
--- src/main/kotlin/SlashCommandListener.kt
+++ src/main/kotlin/SlashCommandListener.kt
@@ -1,8 +1,10 @@
 package com.github.tacticallaptopbag.email_blaster
 
+import net.dv8tion.jda.api.entities.channel.ChannelType
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.OptionMapping
+import org.slf4j.LoggerFactory
 
 class SlashCommandListener(
     private val _messageListener: MessageReceiveListener,
@@ -12,16 +14,30 @@ class SlashCommandListener(
     // https://emailregex.com/
     private val _emailRegex = """(?:[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""".toRegex()
 
+    companion object {
+        private val _logger = LoggerFactory.getLogger(SlashCommandListener::class.java)
+    }
+
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         when(event.name) {
             "setchannel" -> {
                 val channel = event.getOption("channel", OptionMapping::getAsChannel)
+
                 if(channel == null) {
+                    _logger.info("User failed to provide announcement channel")
+
                     event.reply("You must supply a channel!")
                         .setEphemeral(true)
                         .queue()
+                } else if(channel.type != ChannelType.TEXT) {
+                    _logger.info("User failed to provide a text channel")
+
+                    event.reply("You must supply a text channel!")
+                        .setEphemeral(true)
+                        .queue()
                 } else {
-                    _messageListener.announcementChannelId = channel.id
+                    _logger.info("Setting announcement channel to ${channel.id}")
+                    _messageListener.setChannelId(event.guild!!.id, channel.id)
                     event.reply("Successfully set announcement channel to <#${channel.id}>")
                         .queue()
                 }
@@ -29,16 +45,19 @@ class SlashCommandListener(
             "addemail" -> {
                 val email = event.getOption("email", OptionMapping::getAsString)
                 if(email == null) {
+                    _logger.info("User failed to provide an email to add")
                     event.reply("You must supply an email to add!")
                         .setEphemeral(true)
                         .queue()
                 } else {
                     if(email.matches(_emailRegex)) {
+                        _logger.info("Adding email to the mailing list")
                         _mailingList.add(event.guild!!.id, email)
                         event.reply("Added \"$email\" to the mailing list.")
                             .setEphemeral(true)
                             .queue()
                     } else {
+                        _logger.info("User tried to add an invalid email")
                         event.reply("The supplied email is in an incorrect format!")
                             .setEphemeral(true)
                             .queue()
@@ -48,15 +67,18 @@ class SlashCommandListener(
             "removeemail" -> {
                 val email = event.getOption("email", OptionMapping::getAsString)
                 if(email == null) {
+                    _logger.info("User failed to provide an email to remove")
                     event.reply("You must supply an email to remove!")
                         .setEphemeral(true)
                         .queue()
                 } else {
                     if(_mailingList.remove(event.guild!!.id, email)) {
+                        _logger.info("User removed an email from the mailing list")
                         event.reply("Successfully removed \"$email\" from the mailing list.")
                             .setEphemeral(true)
                             .queue()
                     } else {
+                        _logger.info("User tried to remove an email from the mailing list, but it was not there.")
                         event.reply("Could not find the given email in the mailing list!")
                             .setEphemeral(true)
                             .queue()
