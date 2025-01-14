c24e1448416dce8f65c1e151dd6bd05ea429d641
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2025-01-14 11:57:54 -0500

    Using HTML in email
    Also add users to BCC instead of To, to enhance privacy of email messages

--- src/main/kotlin/EmailSession.kt
+++ src/main/kotlin/EmailSession.kt
@@ -1,14 +1,20 @@
 package com.github.tacticallaptopbag.email_blaster
 
 import org.apache.commons.mail.HtmlEmail
-import java.util.*
+import org.slf4j.LoggerFactory
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.stream.Collectors
 
 class EmailSession(
     private val _subject: String,
     private val _message: String,
-    private val _html: Boolean = false,
 ) {
     private val _email = HtmlEmail()
+
+    companion object {
+        private val _logger = LoggerFactory.getLogger(EmailSession::class.java)
+    }
 
     init {
         val user = MailBlasterProperties.emailUser
@@ -19,21 +25,35 @@ class EmailSession(
         _email.setAuthentication(user, password)
         _email.isSSLOnConnect = MailBlasterProperties.emailEnableSSL
         _email.setFrom(user)
-        _email.addBcc(user)
+        _email.addCc(user)
     }
 
     fun addTo(email: String) {
-        _email.addTo(email)
+        _email.addBcc(email)
+    }
+
+    fun sendHTML() {
+        _email.subject = _subject
+
+        val classLoader = Thread.currentThread().contextClassLoader
+        classLoader.getResourceAsStream("email-template.html")?.let { emailTemplateStream ->
+            val emailTemplate = BufferedReader(InputStreamReader(emailTemplateStream))
+                .lines().collect(Collectors.joining("\n"))
+
+            // TODO: Parse markdown in message into HTML
+            val htmlMessage = _message.replace("\n", "\n<br>\n")
+            _email.setHtmlMsg(emailTemplate.replace("{{CONTENT}}", htmlMessage))
+        } ?: {
+            _logger.warn("Unable to find email-template.html in resources. This message will not be sent as HTML.")
+            _email.setMsg(_message)
+        }
+
+        _email.send()
     }
 
     fun send() {
         _email.subject = _subject
-        if(_html) {
-            _email.setHtmlMsg(_message)
-        } else {
-            _email.setTextMsg(_message)
-        }
-
+        _email.setMsg(_message)
         _email.send()
     }
 }
